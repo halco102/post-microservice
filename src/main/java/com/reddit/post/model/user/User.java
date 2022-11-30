fd73f17294442487fb9906729d773500a1bcf2e6
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-11-30 12:40:45 +0100

    adding email to User

--- src/main/java/com/reddit/post/model/user/User.java
+++ src/main/java/com/reddit/post/model/user/User.java
@@ -25,6 +25,9 @@ public class User {
     @Column(name = "username")
     private String username;
 
+    @Column(name = "email")
+    private String email;
+
     @Column(name = "image_url")
     private String imageUrl;
 
