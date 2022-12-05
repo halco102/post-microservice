e74932080d8edb5b0e88dd1c633404e0941e6ed4
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-12-05 13:30:06 +0100

    post like and dislike

--- src/main/java/com/reddit/post/service/post/ILikeOrDislikePost.java
+++ src/main/java/com/reddit/post/service/post/ILikeOrDislikePost.java
@@ -1,4 +1,9 @@
 package com.reddit.post.service.post;
 
+import com.reddit.post.dto.post.PostDto;
+
 public interface ILikeOrDislikePost {
+
+    PostDto likeOrDislikePost(boolean likeOrDislike, Long postId);
+
 }
