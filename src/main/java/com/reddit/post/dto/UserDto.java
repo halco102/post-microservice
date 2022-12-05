6a244fe45e307f46d78df51766bde4d7c7681236
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-12-05 13:33:40 +0100

    user dto with posts as a set (this dto will be used for FE)

---
+++ src/main/java/com/reddit/post/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.reddit.post.dto;
+
+import com.reddit.post.dto.post.PostDto;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import message.PostedBy;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+public class UserDto extends PostedBy {
+
+    private Set<PostDto> postDtos;
+
+    public UserDto(Long id, String email, String username, String imageUrl, Set<PostDto> postDtos) {
+        super(id, email, username, imageUrl);
+        this.postDtos = postDtos;
+    }
+
+}
