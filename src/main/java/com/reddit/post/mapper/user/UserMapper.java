5345e0674e16e89d27c98724bb0b6e962a7968a5
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-12-05 13:34:53 +0100

    user mapper

--- src/main/java/com/reddit/post/mapper/user/UserMapper.java
+++ src/main/java/com/reddit/post/mapper/user/UserMapper.java
@@ -1,9 +1,16 @@
 package com.reddit.post.mapper.user;
 
+import com.reddit.post.dto.UserDto;
+import com.reddit.post.mapper.DoIgnore;
 import com.reddit.post.model.user.User;
 import message.PostedBy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.Qualifier;
 import org.mapstruct.factory.Mappers;
+
+import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -13,5 +20,17 @@ public interface UserMapper {
     PostedBy entityToPostedBy(User user);
 
     User postedByToEntity(PostedBy postedBy);
+
+    Set<PostedBy> fromUserSetToSetDto(Set<User> users);
+
+    @DoIgnore
+    Set<UserDto> temp(Set<User> users);
+
+
+    @DoIgnore
+    @Mappings({
+            @Mapping(source = "posts", target = "postDtos")
+    })
+    UserDto temp2(User user);
 
 }
