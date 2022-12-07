c38a16175e93ee8a803401c93a5c3faf0755e413
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-12-07 19:56:10 +0100

    feign new user apis

--- src/main/java/com/reddit/post/feign/controller/UserClient.java
+++ src/main/java/com/reddit/post/feign/controller/UserClient.java
@@ -1,16 +1,23 @@
 package com.reddit.post.feign.controller;
 
+import com.reddit.post.dto.user.UserSecurityDto;
 import message.PostedBy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "postController", url = "localhost:8086/api/v1/user")
+@FeignClient(name = "userClient", url = "http://localhost:8086/api/v1/user")
 public interface UserClient {
 
 
-    @GetMapping("postedBy/{id}")
+    @GetMapping("/postedBy/{id}")
     PostedBy getPostedByDtoByUserId(@PathVariable("id") Long id);
 
+    @GetMapping("/current")
+    PostedBy getCurrentlyLoggedUser();
+
+    @GetMapping("/security/find-by-username")
+    PostedBy findUserByUsername(@RequestParam("username") String username);
 
 }
