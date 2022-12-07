3db35455d6c4f1970ac102c6988d9b85d2e29acc
Admir Halilovic <halilovic.admir1002@gmail.com>
2022-12-07 19:54:08 +0100

    get user entity from db

--- src/main/java/com/reddit/post/service/user/IUser.java
+++ src/main/java/com/reddit/post/service/user/IUser.java
@@ -1,9 +1,13 @@
 package com.reddit.post.service.user;
 
 import com.reddit.post.model.user.User;
+import message.PostedBy;
 
 public interface IUser {
 
     User saveUser(User user);
 
+    PostedBy getUserById(Long id);
+
+    User getUserEntityById(Long id);
 }
--- src/main/java/com/reddit/post/service/user/UserService.java
+++ src/main/java/com/reddit/post/service/user/UserService.java
@@ -2,6 +2,7 @@ package com.reddit.post.service.user;
 
 import com.reddit.post.exception.BadRequestException;
 import com.reddit.post.exception.NotFoundException;
+import com.reddit.post.mapper.user.UserMapper;
 import com.reddit.post.model.user.User;
 import com.reddit.post.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,10 @@ import org.springframework.stereotype.Service;
 public class UserService implements IUser{
 
     private final UserRepository userRepository;
+
+    private final UserMapper userMapper;
+
+
 
     @Override
     public User saveUser(User user) {
@@ -53,4 +58,14 @@ public class UserService implements IUser{
         log.info("Post service -> delete user by id");
     }
 
+    @Override
+    public PostedBy getUserById(Long id) {
+        var fetchUser =  userRepository.findById(id).orElseThrow(() -> new NotFoundException("User does not exist"));
+        return userMapper.entityToPostedBy(fetchUser);
+    }
+
+    @Override
+    public User getUserEntityById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("The user does not exist"));
+    }
 }
